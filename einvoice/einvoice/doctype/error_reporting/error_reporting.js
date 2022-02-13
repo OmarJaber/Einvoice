a510c1c52c25f15a5150550b6d6212c4b01ca474
root <ahmad18189@gmail.com>
2022-02-13 09:07:40 +0000

    edits

---
+++ einvoice/einvoice/doctype/error_reporting/error_reporting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Omar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Error Reporting', {
+	// refresh: function(frm) {
+
+	// }
+});
